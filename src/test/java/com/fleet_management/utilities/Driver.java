83275b6084d54c238af53f75a9ab22d8ce960161
Ismailduman <issmailduman@gmail.com>
2023-07-20 15:54:37 +0200

    ip adress

--- src/test/java/com/fleet_management/utilities/Driver.java
+++ src/test/java/com/fleet_management/utilities/Driver.java
@@ -28,7 +28,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "54.172.26.43";
+                        String gridAddress = "54.167.76.222";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
