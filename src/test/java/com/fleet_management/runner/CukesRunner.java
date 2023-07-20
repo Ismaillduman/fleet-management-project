3a42f1335dac4f83d5f3612477a844426dc0d62c
Ismailduman <issmailduman@gmail.com>
2023-07-20 15:15:07 +0200

    junit report

--- src/test/java/com/fleet_management/runner/CukesRunner.java
+++ src/test/java/com/fleet_management/runner/CukesRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-
+                "json:target/cucumber.json",
                 "html:target/cucumber-reports.html",
                 "junit:target/junit/junit-report.xml",
                 "rerun:target/rerun.txt",
